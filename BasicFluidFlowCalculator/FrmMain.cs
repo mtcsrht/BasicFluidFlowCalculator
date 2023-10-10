a82680905fd7214e69e23a01d0b835cfff7f1e4c
Mate <mate@localhost.hu>
2023-10-10 11:54:34 +0200

    Ötletek

--- BasicFluidFlowCalculator/FrmMain.cs
+++ BasicFluidFlowCalculator/FrmMain.cs
@@ -28,14 +28,20 @@ namespace BasicFluidFlowCalculator
             //Q = A*v
             //Q*s = output
 
-            var radius = (diameter / 2.0)/100.0;
-            var A = Math.PI * Math.Pow(radius, 2);
-            var Q = A * velocity;
-            var result = Q * time;
+            var radius = (diameter / 2.0)/100.0; //sugár cm to m
+            var A = Math.PI * Math.Pow(radius, 2); //Keresztmetszet m2
+            var Q = A * velocity; //térfogatáram köbméter/másodperc
+            var result = Q * time; // eredmény
 
             var convertingtoDl = (result * 1000) * 10;
 
             txtOutput.Text = convertingtoDl.ToString();
+            //android 8.1 oreo
+            //unity gitignore
+            //Mixer alkalmazás
+            //százalékos kiértékelés, hogy mekkora pontossággal sikerült kitölteni az egyes összetevőket
+            //több koktél recept
+            //viszkozitás és nyomáskülönbség presetek.
         }
     }
 }
